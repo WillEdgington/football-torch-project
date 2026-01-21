8c2635bde7ad13e15c4c46e96e4277da14ee4eb2
WillEdgington <willedge037@gmail.com>
2026-01-21 13:34:10 +0000

    more leagues in LEAGUES dict in fbref_scraper.config

--- fbref_scraper/config.py
+++ fbref_scraper/config.py
@@ -4,19 +4,37 @@ RATELIMITSECONDS = 6
 
 LEAGUES = {
     "Premier-League": "9", # england
-    "Championship": "10",
+    "Championship": "10", # england 2nd div
     "La-Liga": "12", # spain
-    "Segunda-Division": "17",
+    "Segunda-Division": "17", # spain 2nd div
     "Serie-A": "11", # italy
-    "Serie-B": "18",
+    "Serie-B": "18", # italy 2nd div
     "Bundesliga": "20", # germany
-    "2-Bundesliga": "33",
+    "2-Bundesliga": "33", # germany 2nd div
     "Ligue-1": "13", # france
-    "Ligue-2": "60",
+    "Ligue-2": "60", # france 2nd div
     "Belgian-Pro-League": "37", # belgium
-    "Challenger-Pro-League": "69",
-    "Super-Lig": "26", # turkey (no data for 2nd div)
-    "Primeira-Liga": "32", # portugal (no data for 2nd div)
+    "Challenger-Pro-League": "69", # belgium 2nd div
+    "Super-Lig": "26", # turkey
+    "Primeira-Liga": "32", # portugal
+    "Austrian-Bundesliga": "56", # austria
+    "Danish-Superliga": "50", # denmark
+    "Eredivisie": "23", # nederlands
+    "Super-League-Greece": "27", # greece
+    "Czech-First-League": "66", # czech
+    "Scottish-Premiership": "40", # scotland
+    "Ukrainian-Premier-League": "39", # ukraine
+    "Serbian-SuperLiga": "54", # serbia
+    "Hrvatska-NL": "63", # croatia
+    "Swiss-Super-League": "57", # switzerland
+    "A-League-Men": "65", # australia
+    "Bulgarian-First-League": "67", # bulgaria
+    "NB-I": "46", # hungary
+    "Liga-MX": "31", # mexico
+    "Saudi-Pro-League": "70", # saudi arabia
+    "South-African-Premiership": "52", # south africa
+    "Champions-League": "8", # uefa europe
+    "Europa-League": "19", # uefa europe 2nd div
 }
 
 MATCHMETADATASCHEMA = {
